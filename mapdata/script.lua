22d5673d3309afbad073c86cc6fa1799046ac7ba
LynxarA <lynxaradev@gmail.com>
2025-05-09 17:15:33 +0200

    Update script.lua

--- mapdata/script.lua
+++ mapdata/script.lua
@@ -1,3 +1,10 @@
 RegisterNetEvent("prompt:mapdata_sendList", function(returnevent)
+    if Debug == true then
+        print("Getting maps in mapdata (return event) To: ", returnevent)
+    end
     TriggerEvent(returnevent, Maps)
+end)
+
+RegisterNetEvent("prompt:mapdata_exists", function(returnValue)
+    returnValue(true)
 end)
