a55eaa3b91e025553950d4ae9663cabd6459a7c1
LynxarA <lynxaradev@gmail.com>
2025-05-09 12:00:54 +0200

    Update script.lua

--- mapdata/script.lua
+++ mapdata/script.lua
@@ -1,31 +1,18 @@
-local maps = Maps
-
-local events = {}
-
 CreateThread(function()
-    local exists = false 
-    TriggerEvent("lyn-mapdata:exists", function(exists)
-        if exists then
-            exists = true
-        end
+    -- pcalling an event to check if mapdata already exists (prompt:mapdata_initialized). If exists, not creating event
+    local error = pcall(function()
+        TriggerEvent("prompt:mapdata_initialized")
     end)
+    if not error then
+        -- Creating event to check if mapdata already exists (prompt:mapdata_initialized)
+        RegisterNetEvent("prompt:mapdata_initialized", function()
+            -- do something
+        end)
 
-    if exists == true then 
-        print("^6[Prompt]^1 Map data already exists. There must be only one mapdata installed!^0")
+        RegisterNetEvent("prompt:mapdata_sendList", function(returnevent)
+            TriggerEvent(returnevent, Maps)
+        end)
+    else 
+        print("Mapdata already exists somewhere, you have multiple mapdata scripts loaded.")
     end
 end)
-
-RegisterNetEvent("lyn-mapdata:exists", function(cb)
-    cb(true)
-end)
-
-for i = 1, #maps do
-    local eventName = maps[i] .. ":mapDataExists"
-    if Debug == true then 
-        print("Creating event: ", eventName)
-    end
-    local event = RegisterNetEvent(eventName, function(cb) 
-        cb(true)
-    end)
-    table.insert(events, event)
-end
