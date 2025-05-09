ca52b4d8aab64fd727cb119605ee456303ce2d54
LynxarA <lynxaradev@gmail.com>
2025-05-09 21:08:43 +0200

    Update script.lua

--- mapdata/script.lua
+++ mapdata/script.lua
@@ -1,3 +1,28 @@
+Maps = {
+    "Sheriff"
+}
+
+CreateThread(function()
+    local exists = false 
+    TriggerEvent("prompt:mapdata_exists", function(varExists)
+        exists = varExists
+    end)
+
+    local legacy = false 
+    TriggerEvent("lyn-mapdata:exists", function(res)
+        legacy = res
+    end)
+
+    Wait(500)
+    if exists == true then 
+        print("^8 !!!! You have multiple instances of mapdata that can cause incompatibility issues !!!!^7")
+    end
+
+    if legacy == true then 
+        print("^8 !!!! You have legacy mapdata installed (old version), please consider deleting it so script can suggest the new version")
+    end
+end)
+
 RegisterNetEvent("prompt:mapdata_sendList", function(returnevent)
     if Debug == true then
         print("Getting maps in mapdata (return event) To: ", returnevent)
