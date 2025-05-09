f9e127bbea71a07d138a2759e51526136fa2dd3f
LynxarA <lynxaradev@gmail.com>
2025-05-09 12:07:34 +0200

    Update script.lua

--- mapdata/script.lua
+++ mapdata/script.lua
@@ -1,19 +1,3 @@
-CreateThread(function()
-    -- pcalling an event to check if mapdata already exists (prompt:mapdata_initialized). If exists, not creating event
-    local error = pcall(function()
-        TriggerEvent("prompt:mapdata_initialized")
-    end)
-        print(error)
-    if not error then
-        -- Creating event to check if mapdata already exists (prompt:mapdata_initialized)
-        RegisterNetEvent("prompt:mapdata_initialized", function()
-            -- do something
-        end)
-
-        RegisterNetEvent("prompt:mapdata_sendList", function(returnevent)
-            TriggerEvent(returnevent, Maps)
-        end)
-    else 
-        print("Mapdata already exists somewhere, you have multiple mapdata scripts loaded.")
-    end
+RegisterNetEvent("prompt:mapdata_sendList", function(returnevent)
+    TriggerEvent(returnevent, Maps)
 end)
