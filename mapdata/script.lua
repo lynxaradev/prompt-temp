11bf53c98cc466dc28a2d6f3756fdf98afef294d
LynxarA <lynxaradev@gmail.com>
2025-05-10 13:20:06 +0200

    Update script.lua

--- mapdata/script.lua
+++ mapdata/script.lua
@@ -1,7 +1,3 @@
-Maps = {
-    "Sheriff"
-}
-
 CreateThread(function()
     local exists = false 
     TriggerEvent("prompt:mapdata_exists", function(varExists)
@@ -20,6 +16,72 @@ CreateThread(function()
 
     if legacy == true then 
         print("^8 !!!! You have also a legacy mapdata version installed (old version) alongside the new one, please consider deleting it so script can run the new version")
+    end
+
+    -- Legacy check
+    local legacyMaps = {}
+    for i = 1, #Maps do 
+        local legacyCheckName = Maps[i].. ":mapExists"
+        local legacyExists = false
+
+        TriggerEvent(legacyCheckName, function(existsCB)
+            legacyExists = existsCB
+        end)
+        Wait(100)
+
+        if legacyExists == true then
+            if Debug == true then
+                print("Found legacy map: ", Maps[i])
+            end
+            table.insert(legacyMaps, Maps[i])
+        end
+    end
+
+    if #legacyMaps > 0 then
+        if Debug == true then 
+            print("Turning on legacy support")
+        end
+
+        local newMaps = {}
+        for i = 1, #Maps do 
+            local isLegacy = false 
+            for j = 1, #legacyMaps do
+                if legacyMaps[j] == Maps[i] then
+                    isLegacy = true
+                    break
+                end
+            end
+
+            if isLegacy == false then
+                table.insert(newMaps, Maps[i])
+            end
+        end
+
+        for i = 1, #newMaps do
+            if Debug == true then
+                print("Adding fake events for: ", newMaps[i])
+            end
+            local existsName = newMaps[i].. ":mapExists"
+            local nameSend = newMaps[i].. ":mapFullNameSend"
+            local mapdataExists = newMaps[i].. ":mapDataExists"
+            local final = newMaps[i].. ":mapFinal"
+
+            RegisterNetEvent(existsName, function(cb)
+                cb(true)
+            end)
+
+            RegisterNetEvent(nameSend, function(returnEvent, id)
+                TriggerEvent(returnEvent, newMaps[i], id)
+            end)
+                
+            RegisterNetEvent(mapdataExists, function(cb)
+                cb(true)
+            end)
+
+            RegisterNetEvent(final, function()
+                -- do nothing
+            end)
+        end
     end
 end)
 
