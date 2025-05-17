f895b3d5aeeb3e44afa18a07617d4288c4c9c0fa
LynxarA <lynxaradev@gmail.com>
2025-05-17 21:56:03 +0200

    Update script.lua

--- mapdata/script.lua
+++ mapdata/script.lua
@@ -4,6 +4,7 @@ CreateThread(function()
         exists = varExists
     end)
 
+    -- Legacy event support
     local legacy = false 
     TriggerEvent("lyn-mapdata:exists", function(res)
         legacy = res
@@ -15,73 +16,15 @@ CreateThread(function()
         print("^8 !!!! You have also a legacy mapdata version installed (old version) alongside the new one, please consider deleting it so script can run the new version")
     end
 
-    -- Legacy check
-    local legacyMaps = {}
+    -- Legacy mapdata support
     for i = 1, #Maps do 
-        local legacyCheckName = Maps[i].. ":mapExists"
-        local legacyExists = false
-
-        TriggerEvent(legacyCheckName, function(existsCB)
-            legacyExists = existsCB
-        end)
-        Wait(100)
-
-        if legacyExists == true then
-            if Debug == true then
-                print("Found legacy map: ", Maps[i])
-            end
-            table.insert(legacyMaps, Maps[i])
-        end
-    end
-
-    if #legacyMaps > 0 then
         if Debug == true then 
-            print("Turning on legacy support")
+            print("Adding legacy mapdata support for: ", Maps[i])
         end
-
-        local newMaps = {}
-        for i = 1, #Maps do 
-            local isLegacy = false 
-            for j = 1, #legacyMaps do
-                if legacyMaps[j] == Maps[i] then
-                    isLegacy = true
-                    break
-                end
-            end
-
-            if isLegacy == false then
-                table.insert(newMaps, Maps[i])
-            end
-
-            if Debug == true then 
-                print("Adding legacy mapdata support for: ", Maps[i])
-            end
-            local mapdataExists = Maps[i].. ":mapDataExists"
-            RegisterNetEvent(mapdataExists, function(cb)
-                cb(true)
-            end)
-        end
-
-        for i = 1, #newMaps do
-            if Debug == true then
-                print("Adding fake events for: ", newMaps[i])
-            end
-            local existsName = newMaps[i].. ":mapExists"
-            local nameSend = newMaps[i].. ":mapFullNameSend"
-            local final = newMaps[i].. ":mapFinal"
-
-            RegisterNetEvent(existsName, function(cb)
-                cb(true)
-            end)
-
-            RegisterNetEvent(nameSend, function(returnEvent, id)
-                TriggerEvent(returnEvent, newMaps[i], id)
-            end)
-
-            RegisterNetEvent(final, function()
-                -- do nothing
-            end)
-        end
+        local mapdataExists = Maps[i].. ":mapDataExists"
+        RegisterNetEvent(mapdataExists, function(cb)
+            cb(true)
+        end)
     end
 end)
 
